d4f1aa730b34875d01c32c5b69705ac5caad236d
quanwenchao <quan_wc@163.com>
2019-02-01 11:37:28 +0800

    add docker file

--- src/main/java/com/quanwc/WeixinServiceApplication.java
+++ src/main/java/com/quanwc/WeixinServiceApplication.java
@@ -3,6 +3,11 @@ package com.quanwc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 主类：
+ * @SpringBootApplication注解 来表明是一个主程序类，是一个spring boot应用
+ *                            该类是spring boot的主配置类，spring boot就应该运行这个类的main方法来启动spring boot应用
+ */
 @SpringBootApplication
 public class WeixinServiceApplication {
 
